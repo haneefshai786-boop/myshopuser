e9637802c2662318ea05d18dad4ccb47af2407bb
haneefshai786-boop <haneefshai786@gmail.com>
2025-12-13 13:25:26 +0530

    Refactor routing structure in App component
    Reorganized route structure for better clarity and added comments for sections.

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,27 @@ import Subcategories from './pages/Subcategories.jsx';
 import Products from './pages/Products.jsx';
 import ProductDetail from './pages/ProductDetail.jsx';
 import Cart from './pages/Cart';
-export default function App(){
+
+export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<UserLayout/>}>
-        <Route index element={<Home/>} />
-        <Route path="vendors" element={<Vendors/>} />
-        <Route path="vendors/:vendorId" element={<Categories/>} />
-        <Route path="vendors/:vendorId/categories/:categoryId" element={<Subcategories/>} />
-        <Route path="vendors/:vendorId/categories/:categoryId/sub/:subId" element={<Products/>} />
-        <Route path="product/:id" element={<ProductDetail/>} />
+      <Route path="/" element={<UserLayout />}>
+        
+        {/* HOME */}
+        <Route index element={<Home />} />
+
+        {/* FOLDER LEVEL */}
+        <Route path="vendors/:type" element={<Vendors />} />
+
+        {/* FLOW */}
+        <Route path="vendors/:vendorId/categories" element={<Categories />} />
+        <Route path="vendors/:vendorId/categories/:categoryId" element={<Subcategories />} />
+        <Route path="vendors/:vendorId/categories/:categoryId/sub/:subId" element={<Products />} />
+
+        {/* PRODUCT & CART */}
+        <Route path="product/:id" element={<ProductDetail />} />
         <Route path="cart" element={<Cart />} />
+
       </Route>
     </Routes>
   );

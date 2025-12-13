0e50bfb82f1f82d1be20cd9bfd4b050611b57516
haneefshai786-boop <haneefshai786@gmail.com>
2025-12-13 11:59:28 +0530

    Add Cart route to App component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Categories from './pages/Categories.jsx';
 import Subcategories from './pages/Subcategories.jsx';
 import Products from './pages/Products.jsx';
 import ProductDetail from './pages/ProductDetail.jsx';
+import Cart from './pages/Cart';
 export default function App(){
   return (
     <Routes>
@@ -16,6 +17,7 @@ export default function App(){
         <Route path="vendors/:vendorId/categories/:categoryId" element={<Subcategories/>} />
         <Route path="vendors/:vendorId/categories/:categoryId/sub/:subId" element={<Products/>} />
         <Route path="product/:id" element={<ProductDetail/>} />
+        <Route path="cart" element={<Cart />} />
       </Route>
     </Routes>
   );

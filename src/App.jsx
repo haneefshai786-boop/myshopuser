a6ec3bae7d531008a34ba8954c8a54e20a275016
haneefshai786-boop <haneefshai786@gmail.com>
2025-12-14 21:25:25 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-// src/App.jsx
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import UserLayout from './layout/UserLayout.jsx';
 import UserPrivateRoute from './components/UserPrivateRoute.jsx';
 
@@ -16,34 +15,24 @@ import Register from './pages/Register.jsx';
 
 export default function App() {
   return (
-    <>
-      {/* Simple nav links */}
-      <nav style={{ padding: 10, background: '#f0f0f0', marginBottom: 20 }}>
-        <Link to="/" style={{ marginRight: 15 }}>Home</Link>
-        <Link to="/cart" style={{ marginRight: 15 }}>Cart</Link>
-        <Link to="/login" style={{ marginRight: 15 }}>Login</Link>
-        <Link to="/register">Register</Link>
-      </nav>
+    <Routes>
+      {/* Public auth */}
+      <Route path="/login" element={<UserLogin />} />
+      <Route path="/register" element={<Register />} />
 
-      <Routes>
-        {/* Public auth pages */}
-        <Route path="/login" element={<UserLogin />} />
-        <Route path="/register" element={<Register />} />
+      {/* Public pages */}
+      <Route path="/" element={<UserLayout />}>
+        <Route index element={<Home />} />
+        <Route path="vendors/:vendorId/categories/:categoryId/sub/:subcategoryId" element={<Products />} />
+        <Route path="product/:productId" element={<ProductDetail />} />
+        <Route path="cart" element={<Cart />} />
+      </Route>
 
-        {/* Public pages */}
-        <Route path="/" element={<UserLayout />}>
-          <Route index element={<Home />} />
-          <Route path="vendors/:vendorId/categories/:categoryId/sub/:subcategoryId" element={<Products />} />
-          <Route path="product/:productId" element={<ProductDetail />} />
-          <Route path="cart" element={<Cart />} />
-        </Route>
-
-        {/* Protected pages */}
-        <Route element={<UserPrivateRoute />}>
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/orders" element={<MyOrders />} />
-        </Route>
-      </Routes>
-    </>
+      {/* Protected pages */}
+      <Route element={<UserPrivateRoute />}>
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/orders" element={<MyOrders />} />
+      </Route>
+    </Routes>
   );
 }

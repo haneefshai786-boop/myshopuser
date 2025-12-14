49345faac4f1b57b29bcc1fdcf0a7f2c3ede416a
haneefshai786-boop <haneefshai786@gmail.com>
2025-12-14 21:33:51 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://myshopbackend-gj8m.onrender.com/api'
+  baseURL: 'https://myshopbackend-gj8m.onrender.com/api',
 });
 
+// Automatically attach token to requests if exists
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('userToken');
   if (token) config.headers.Authorization = `Bearer ${token}`;

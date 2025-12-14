0edabf0b34c3e0dc8605d65551a760108b448dae
haneefshai786-boop <haneefshai786@gmail.com>
2025-12-14 21:31:27 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,26 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import api from '../api.js';
-
+import api from '../api';
 
 export default function Home() {
-const [vendors, setVendors] = useState([]);
+  const [vendors, setVendors] = useState([]);
 
+  useEffect(() => {
+    api.get('/vendors').then(res => setVendors(res.data));
+  }, []);
 
-useEffect(() => {
-api.get('/vendors').then(res => setVendors(res.data));
-}, []);
-
-
-return (
-<div>
-<h1>Vendors</h1>
-{vendors.map(v => (
-<div key={v._id} style={{ margin:10, padding:10, border:'1px solid #ccc' }}>
-<h3>{v.name}</h3>
-<p>{v.description}</p>
-<Link to={`/vendors/${v._id}/categories`}>View Categories</Link>
-</div>
-))}
-</div>
-);
+  return (
+    <div>
+      <h1>Vendors</h1>
+      <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fill,minmax(200px,1fr))', gap: 15 }}>
+        {vendors.map(v => (
+          <div key={v._id} style={{ padding: 15, border: '1px solid #ccc', borderRadius: 8 }}>
+            <h3>{v.name}</h3>
+            <p>{v.description}</p>
+            <Link to={`/vendors/${v._id}/categories`}>View Categories</Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }

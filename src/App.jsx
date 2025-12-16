a83e3c6f3e524d824cb154e8186a74f14ce3eae8
haneefshai786-boop <haneefshai786@gmail.com>
2025-12-16 08:05:45 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,43 @@
-import { Routes, Route } from 'react-router-dom';
-import UserLayout from './layout/UserLayout.jsx';
-import UserPrivateRoute from './components/UserPrivateRoute.jsx';
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import UserPrivateRoute from "./components/UserPrivateRoute";
 
-import Home from './pages/Home.jsx';
-import Categories from './pages/Categories.jsx';
-import Subcategories from './pages/Subcategories.jsx';
-import Products from './pages/Products.jsx';
-import ProductDetail from './pages/ProductDetail.jsx';
-import Cart from './pages/Cart.jsx';
-import Checkout from './pages/Checkout.jsx';
-import MyOrders from './pages/MyOrders.jsx';
-import UserLogin from './pages/UserLogin.jsx';
-import Register from './pages/Register.jsx';
+import Login from "./pages/Login";
+import Products from "./pages/Products";
+import Cart from "./pages/Cart";
+import Orders from "./pages/Orders";
 
 export default function App() {
   return (
-    <Routes>
-      {/* Public auth */}
-      <Route path="/login" element={<UserLogin />} />
-      <Route path="/register" element={<Register />} />
-
-      {/* Public pages */}
-      <Route path="/" element={<UserLayout />}>
-        <Route index element={<Home />} />
-        <Route path="vendors/:vendorId/categories/:categoryId/sub/:subcategoryId" element={<Products />} />
-        <Route path="product/:productId" element={<ProductDetail />} />
-        <Route path="cart" element={<Cart />} />
-      </Route>
-
-      {/* Protected pages */}
-      <Route element={<UserPrivateRoute />}>
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/orders" element={<MyOrders />} />
-      </Route>
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/"
+          element={
+            <UserPrivateRoute>
+              <Products />
+            </UserPrivateRoute>
+          }
+        />
+        <Route
+          path="/cart"
+          element={
+            <UserPrivateRoute>
+              <Cart />
+            </UserPrivateRoute>
+          }
+        />
+        <Route
+          path="/orders"
+          element={
+            <UserPrivateRoute>
+              <Orders />
+            </UserPrivateRoute>
+          }
+        />
+      </Routes>
+    </>
   );
-}
+          }
